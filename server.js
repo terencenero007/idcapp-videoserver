ca62f0e40bbeece629fbcc0e782ec16cbf9c841f
unknown <P10267646@COS-LPT-DDAS.capita.co.in>
2016-07-09 17:17:22 +0530

    Corrected webm recording

--- server.js
+++ server.js
@@ -2,35 +2,35 @@
 var express = require('express'),
 	SocketServer = require('ws').Server;
 
-// Initialize port from environment
+// Initialize the port from the environment variable
 // Let 3000 by default
 var PORT = process.env.PORT || 3000;
 
-// Initialize express on given port
+// Initialize the server on the given port
 var app = express();
-	server = app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+server = app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-// Initialize web socket
+// Initialize web socket on the server
 var socketServer = new SocketServer({ server });
 
-// Initialize connection from socket client	
+// Initialize connection request from a socket client	
 socketServer.on('connection', (socket) => {
-	console.log('New socket connection ('+socketServer.clients.length+' total)');
-	
-	// Client is closing
-  	socket.on('close', function(code, message){
-		console.log('Disconnected socket ('+socketServer.clients.length+' total)');
+	console.log('New socket connection (' + socketServer.clients.length + ' total)');
+
+	// Close notification from a client
+	socket.on('close', function (code, message) {
+		console.log('Disconnected socket (' + socketServer.clients.length + ' total)');
 	});
 });
 
-// Broadcast the given data to all the connected clients in the socket
-socketServer.broadcast = function(data, opts) {
-	for( var i in this.clients ) {
+// Broadcast the given data to all the connected clients
+socketServer.broadcast = function (data, opts) {
+	for (var i in this.clients) {
 		if (this.clients[i].readyState == 1) {
 			this.clients[i].send(data, opts);
 		}
 		else {
-			console.log('Error: client ('+i+') not connected');
+			console.log('Error: client (' + i + ') not connected');
 		}
 	}
 };
@@ -40,36 +40,33 @@ app.use(express.static(__dirname + "/"))
 
 // default GET API
 app.get('/', function (req, res) {
-  res.send('IDC video server is up...');
+	res.send('IDC video server is up...');
 });
 
-// Streaming API to accept incoming stream
-/*app.post("/video", function (req, res) {
-	console.log('Stream connected: ' +  req.socket.remoteAddress + ':' +  req.socket.remotePort);
-		
-	// Pass the icoming data to the socket
-	req.on('data', function(data){
-		console.log(data);
-		socketServer.broadcast(data, {binary:true});
-	});
-	
-	res.sendStatus(200);
-});*/
+// Streaming API to accept incoming video stream and broadcast to all the connected clients
+app.post("/video", function (req, res) {
+	console.log('Stream connected: ' + req.socket.remoteAddress + ':' + req.socket.remotePort);
 
-// Streaming API to accept incoming stream
-app.use("/video", function (req, res) {
-	res.connection.setTimeout(0);
-	
-	console.log(
-			'Stream connected: ' + 
-			req.socket.remoteAddress + ':' + 
-			req.socket.remotePort
-		);
-		
-	// Pass the icoming data to the socket
-	req.on('data', function(data){
-		console.log(data);
-		// console.log(data.type);
-		socketServer.broadcast(data, {binary:true});
+	// Read stream from the incoming request
+	// Append each chunks while streaming
+	var dataBuffer = null, newBuffer, bufferLength;
+	req.on('data', function (data) {
+		if (dataBuffer == null) {
+			dataBuffer = data;
+		}
+		else {
+			bufferLength = data.length + dataBuffer.length;
+			newBuffer = Buffer.concat([dataBuffer, data], bufferLength);
+			dataBuffer = newBuffer;
+		}
+	});
+
+	// Broadcast data to the connected clients after reading the stream
+	req.on('end', function () {
+		console.log("Stream completed");
+		console.log(dataBuffer.length);
+		console.log(dataBuffer);
+		socketServer.broadcast(dataBuffer, { binary: true });
+		res.sendStatus(200);
 	});
 });
